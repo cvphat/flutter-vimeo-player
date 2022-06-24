4f9aefff3a182b31e27f7e164b6eb2238b0e4cc4
cvphat <cvphat@hotmail.com>
2022-06-24 09:15:38 +0700

    upgrade version fix warning flutter 3

--- lib/src/controllers/vimeo_player_controller.dart
+++ lib/src/controllers/vimeo_player_controller.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:fluttervimeoplayer/flutter_vimeo_player.dart';
 
@@ -15,31 +15,33 @@ class VimeoPlayerValue {
   final double? videoHeight;
   final InAppWebViewController? webViewController;
 
-  VimeoPlayerValue(
-      {this.isReady = false,
-      this.isPlaying = false,
-      this.isFullscreen = false,
-      this.isBuffering = false,
-      this.hasEnded = false,
-      this.videoTitle,
-      this.videoPosition,
-      this.videoDuration,
-      this.videoWidth,
-      this.videoHeight,
-      this.webViewController});
+  VimeoPlayerValue({
+    this.isReady = false,
+    this.isPlaying = false,
+    this.isFullscreen = false,
+    this.isBuffering = false,
+    this.hasEnded = false,
+    this.videoTitle,
+    this.videoPosition,
+    this.videoDuration,
+    this.videoWidth,
+    this.videoHeight,
+    this.webViewController,
+  });
 
-  VimeoPlayerValue copyWith(
-      {bool? isReady,
-      bool? isPlaying,
-      bool? isFullscreen,
-      bool? isBuffering,
-      bool? hasEnded,
-      String? videoTitle,
-      double? videoPosition,
-      double? videoDuration,
-      double? videoWidth,
-      double? videoHeight,
-      InAppWebViewController? webViewController}) {
+  VimeoPlayerValue copyWith({
+    bool? isReady,
+    bool? isPlaying,
+    bool? isFullscreen,
+    bool? isBuffering,
+    bool? hasEnded,
+    String? videoTitle,
+    double? videoPosition,
+    double? videoDuration,
+    double? videoWidth,
+    double? videoHeight,
+    InAppWebViewController? webViewController,
+  }) {
     return VimeoPlayerValue(
         isReady: isReady ?? this.isReady,
         isPlaying: isPlaying ?? this.isPlaying,
